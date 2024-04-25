5e136e6c5027715929c2f0fa1a0aaf9c5f04c8a3
LuciePrejzova <lucie.prejzova@gmail.com>
2024-04-25 10:29:15 +0200

    initial build

---
+++ src/main/java/com/FLsolutions/absenceTracker/AbsenceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.FLsolutions.absenceTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsenceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsenceTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FLsolutions/absenceTracker/AbsenceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FLsolutions.absenceTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbsenceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
